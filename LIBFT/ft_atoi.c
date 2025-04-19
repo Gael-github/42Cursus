cb8a98fdb0a485c63833db5e2cc5ab4ed0144b44
Gael <nobenham@student.42malaga.com>
2025-04-19 23:15:08 +0200

    Update ft_atoi.c

--- LIBFT/ft_atoi.c
+++ LIBFT/ft_atoi.c
@@ -30,8 +30,6 @@ int	ft_atoi(const char *str)
 	}
 	else if (str[i] == '+')
 		i++;
-	while (str[i] == '+' || str[i] == '-')
-		break ;
 	while ((str[i] != '\0') && (str[i] >= '0' && str[i] <= '9'))
 	{
 		res = (res * 10) + (str[i] - '0');
@@ -39,10 +37,3 @@ int	ft_atoi(const char *str)
 	}
 	return (res * sign);
 }
-/*#include <stdio.h>
-
-int main(void)
-{
-    printf("%d", ft_atoi("   --1234v345"));
-    return (0);
-}*/
